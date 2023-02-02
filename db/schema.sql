d4cb8c594d6162451b50f08c84b4c6ae85e37bab
Edwin Hernandez <edwinhernandez80@icloud.com>
2023-02-01 20:10:40 -0800

    created role table

--- db/schema.sql
+++ db/schema.sql
@@ -8,3 +8,17 @@ CREATE TABLE department (
     name VARCHAR(30) NOT NULL
 );
 
+CREATE TABLE role (
+    id INT NOT NULL  AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_ID INT NOT NULL
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL,
+    manager_id 
+)
