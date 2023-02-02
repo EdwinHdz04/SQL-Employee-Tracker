9cdff260992134afaca7df61145ae8bb1802e6a4
Edwin Hernandez <edwinhernandez80@icloud.com>
2023-02-01 21:58:22 -0800

    created express middleware

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const PORT = process.env.PORT || 3001;
 const app = express ();
 
 //Express middleware
-app.use
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+
+//
 
 
 
