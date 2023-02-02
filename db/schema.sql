1f9a5cd8a7961672bf5914df7e54bf2a2f6d0cd9
Edwin Hernandez <edwinhernandez80@icloud.com>
2023-02-01 19:57:08 -0800

    created department table

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+USE company_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
