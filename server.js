721f04b9be321e49b9f8c7c7686a44d115eb4d13
Edwin Hernandez <edwinhernandez80@icloud.com>
2023-02-02 23:15:53 -0800

    updated file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //require mysql2,inquirer and console table
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
-const Connection = require('mysql2/typings/mysql/lib/Connection');
+
 
 require ('console.table');
 
@@ -43,50 +43,49 @@ function prompt(){
         type: 'list',
         message: 'What would you like to do?',
         choices: [
-            promptMessages.viewAllemployees,
-            promptMessages.viewBydepartment,
-            promptMessages.viewBymanager,
-            promptMessages.viewAllroles,
-            promptMessages.addEmployee,
-            promptMessages.removeEmployee,
-            promptMessages.updateRole,
-            promptMessages.exit
-        
+            viewAllemployees,
+            viewBydepartment,
+            viewBymanager,
+            viewAllroles,
+            addEmployee,
+            removeEmployee,
+            updateRole,
+            quit
         ]
      })
      .then(answer => {
         console.log('answer', answer);
             switch ( answer.action){
-                case promptMessages.viewAllemployees:
+                case viewAllemployees:
                     viewAllemployees();
                     break
                 
-                case promptMessages.viewBydepartment:
+                case viewBydepartment:
                     viewBydepartment();
                     break
 
-                case promptMessages.viewBymanager:
+                case viewBymanager:
                     viewBymanager();
                     break
 
-                case promptMessages.addEmployee:
+                case addEmployee:
                     addEmployee();
                     break
 
-                case promptMessages.removeEmployee:
+                case removeEmployee:
                     removeEmployee();
                     break
 
-                case promptMessages.updateRole:
+                case updateRole:
                     updateRole();
                     break
 
-                case promptMessages.viewAllroles:
+                case viewAllroles:
                     viewAllroles();
                     break
 
-                case promptMessages.exit:
-                Connection.end()
+                case exit:
+                    connection.end()
                 break;
 
             }    
@@ -165,3 +164,173 @@ function viewAllroles(){
     });
 
 }
+
+async function addEmployee() {
+    const addname = await inquirer.prompt(askName());
+    connection.query('SELECT roles.id, roles.title FROM roles ORDER BY roles.id;', async (err, res) => {
+        if (err) throw err;
+        const { role } = await inquirer.prompt([
+            {
+                name: 'roles',
+                type: 'list',
+                choices: () => res.map(res => res.title),
+                message: 'What is the employee role?: '
+            }
+        ]);
+        let rolesId;
+        for (const row of res) {
+            if (row.title === roles) {
+                rolesId = row.id;
+                continue;
+            }
+        }
+        connection.query('SELECT * FROM employees', async (err, res) => {
+            if (err) throw err;
+            let choices = res.map(res => `${res.first_name} ${res.last_name}`);
+            choices.push('none');
+            let { manager } = await inquirer.prompt([
+                {
+                    name: 'manager',
+                    type: 'list',
+                    choices: choices,
+                    message: 'Choose the employee Manager: '
+                }
+            ]);
+            let managerId;
+            let managerName;
+            if (manager === 'none') {
+                managerId = null;
+            } else {
+                for (const data of res) {
+                    data.fullName = `${data.first_name} ${data.last_name}`;
+                    if (data.fullName === manager) {
+                        managerId = data.id;
+                        managerName = data.fullName;
+                        console.log(managerId);
+                        console.log(managerName);
+                        continue;
+                    }
+                }
+            }
+            console.log('Employee has been added. Please view all employee to verify...');
+            connection.query(
+                'INSERT INTO employee SET ?',
+                {
+                    first_name: addname.first,
+                    last_name: addname.last,
+                    roles_id: rolesId,
+                    manager_id: parseInt(managerId)
+                },
+                (err, res) => {
+                    if (err) throw err;
+                    prompt();
+
+                }
+            );
+        });
+    });
+
+}
+function remove(input) {
+    const promptQ = {
+        yes: "yes",
+        no: "no I don't (view all employees on the main option)"
+    };
+    inquirer.prompt([
+        {
+            name: "action",
+            type: "list",
+            message: "In order to proceed an employee, an ID must be entered. View all employees to get" +
+                " the employee ID. Do you know the employee ID?",
+            choices: [promptQ.yes, promptQ.no]
+        }
+    ]).then(answer => {
+        if (input === 'delete' && answer.action === "yes") removeEmployee();
+        else if (input === 'role' && answer.action === "yes") updateRole();
+        else viewAllEmployees();
+
+
+
+    });
+};
+
+async function removeEmployee() {
+
+    const answer = await inquirer.prompt([
+        {
+            name: "first",
+            type: "input",
+            message: "Enter the employee ID you want to remove:  "
+        }
+    ]);
+
+    connection.query('DELETE FROM employee WHERE ?',
+        {
+            id: answer.first
+        },
+        function (err) {
+            if (err) throw err;
+        }
+    )
+    console.log('Employee has been removed on the system!');
+    prompt();
+
+};
+
+function askId() {
+    return ([
+        {
+            name: "name",
+            type: "input",
+            message: "What is the employee ID?:  "
+        }
+    ]);
+}
+
+
+async function updateRole() {
+    const employeeId = await inquirer.prompt(askId());
+
+    connection.query('SELECT roles.id, roles.title FROM roles ORDER BY roles.id;', async (err, res) => {
+        if (err) throw err;
+        const { role } = await inquirer.prompt([
+            {
+                name: 'role',
+                type: 'list',
+                choices: () => res.map(res => res.title),
+                message: 'What is the new employee role?: '
+            }
+        ]);
+        let roleId;
+        for (const row of res) {
+            if (row.title === roles) {
+                rolesId = row.id;
+                continue;
+            }
+        }
+        connection.query(`UPDATE employee 
+        SET role_id = ${roleId}
+        WHERE employee.id = ${employeeId.name}`, async (err, res) => {
+            if (err) throw err;
+            console.log('Role has been updated..')
+            prompt();
+        });
+    });
+}
+
+function askName() {
+    return ([
+        {
+            name: "first",
+            type: "input",
+            message: "Enter the first name: "
+        },
+        {
+            name: "last",
+            type: "input",
+            message: "Enter the last name: "
+        }
+    ]);
+}
+
+
